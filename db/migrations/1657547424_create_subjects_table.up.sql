664387b4f2d0b65c2044ac166124324d3a1269f9
Ricardo Lüders <rluders@redhat.com>
2022-07-12 09:22:10 +0200

    refactor(database): subjects table up

--- db/migrations/1657547424_create_subjects_table.up.sql
+++ db/migrations/1657547424_create_subjects_table.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE subjects
     id              bigserial       CONSTRAINT subjects_pk PRIMARY KEY,
     uuid            uuid            DEFAULT uuid_generate_v4() NOT NULL,
     code            varchar         NOT NULL UNIQUE,
-    title           varchar         NOT NULL,
+    "name"          varchar         NOT NULL,
     objective       text            NULL,
     credit          decimal         NULL,
     workload        decimal         NULL,
@@ -16,6 +16,8 @@ CREATE TABLE subjects
     deleted_at      timestamp
 );
 
+CREATE UNIQUE INDEX subjects_id_uindex
+    ON subjects (id);
 CREATE UNIQUE INDEX subjects_uuid_uindex
     ON subjects (uuid);
 
