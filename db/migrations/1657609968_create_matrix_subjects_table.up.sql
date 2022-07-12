2ebd3066a1032b8fa372971ccc8a7cb3934c5890
Ricardo Lüders <rluders@redhat.com>
2022-07-12 09:22:28 +0200

    refactor(database): matrix subjects table migration

---
+++ db/migrations/1657609968_create_matrix_subjects_table.up.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE matrix_subjects
+(
+    id              bigserial       CONSTRAINT matrix_subjects_pk PRIMARY KEY,
+    subject_id      bigserial       NOT NULL,
+    matrix_id       bigserial       NOT NULL,
+    is_required     boolean         NULL DEFAULT TRUE,
+    created_at      timestamp       DEFAULT now() NOT NULL,
+    updated_at      timestamp       DEFAULT now() NOT NULL,
+    deleted_at      timestamp
+);
+
+CREATE UNIQUE INDEX matrix_subjects_id_uindex
+    ON matrix_subjects (id);
+
+COMMIT;
