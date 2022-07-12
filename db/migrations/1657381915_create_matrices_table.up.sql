b77251676b06199715989ff61656b351cffb9d89
Ricardo Lüders <rluders@redhat.com>
2022-07-12 09:21:52 +0200

    refactor(database): matrices table up

--- db/migrations/1657381915_create_matrices_table.up.sql
+++ db/migrations/1657381915_create_matrices_table.up.sql
@@ -6,14 +6,17 @@ CREATE TABLE matrices
 (
     id              bigserial       CONSTRAINT matrices_pk PRIMARY KEY,
     uuid            uuid            DEFAULT uuid_generate_v4() NOT NULL,
-    title           varchar         NOT NULL,
-    description     text,
+    code            varchar         NOT NULL UNIQUE,
+    "name"          varchar         NOT NULL,
+    description     text            NULL,
     course_id       uuid            NOT NULL,
     created_at      timestamp       DEFAULT now() NOT NULL,
     updated_at      timestamp       DEFAULT now() NOT NULL,
     deleted_at      timestamp
 );
 
+CREATE UNIQUE INDEX matrices_id_uindex
+    ON matrices (id);
 CREATE UNIQUE INDEX matrices_uuid_uindex
     ON matrices (uuid);
 
