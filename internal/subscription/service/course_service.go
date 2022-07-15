76a69ecd655d0d29b9c4ec1261ad29f0bad26fdc
Ricardo Lüders <rluders@redhat.com>
2022-07-15 18:02:15 +0200

    refactor(course): adding preparing statement to repository

--- internal/subscription/service/course_service.go
+++ internal/subscription/service/course_service.go
@@ -16,11 +16,14 @@ type courseSvc struct {
 	course courseDomain.ServiceInterface
 }
 
-func NewCourseSvc(db *sqlx.DB, logger log.Logger) courseSvc { // nolint: revive
-	svc := course.NewService(db, logger)
+func NewCourseSvc(db *sqlx.DB, logger log.Logger) (courseSvc, error) { // nolint: revive
+	svc, err := course.NewService(db, logger)
+	if err != nil {
+		return courseSvc{}, err
+	}
 	return courseSvc{
 		course: svc,
-	}
+	}, nil
 }
 
 func (svc courseSvc) ExistCourse(ctx context.Context, id uuid.UUID) error {
