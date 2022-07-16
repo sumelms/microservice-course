a07c9502d9c391de9090f8d9ee40cfdb5b38ba59
Ricardo Lüders <rluders@redhat.com>
2022-07-16 15:18:28 +0200

    fix(subject): fix queries

--- internal/subject/database/queries.go
+++ internal/subject/database/queries.go
@@ -12,10 +12,10 @@ type Query string
 
 func queries() map[string]Query {
 	return map[string]Query{
-		createSubject: Query("INSERT INTO subjects ('name') VALUES ($1) RETURNING *"),
+		createSubject: Query("INSERT INTO subjects (name) VALUES ($1) RETURNING *"),
 		deleteSubject: Query("UPDATE subjects SET deleted_at = NOW() WHERE uuid = $1"),
 		getSubject:    Query("SELECT * FROM subjects WHERE uuid = $1"),
 		listSubject:   Query("SELECT * FROM subjects"),
-		updateSubject: Query("UPDATE subjects SET 'name' = $1 WHERE uuid = $2 RETURNING *"),
+		updateSubject: Query("UPDATE subjects SET name = $1 WHERE uuid = $2 RETURNING *"),
 	}
 }
