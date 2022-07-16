4cb89bcb046b65953b91b717f769d99e20cfc524
Ricardo Lüders <rluders@redhat.com>
2022-07-16 15:19:18 +0200

    fix(course): queries and repository

--- internal/course/database/queries.go
+++ internal/course/database/queries.go
@@ -12,10 +12,10 @@ type Query string
 
 func queries() map[string]Query {
 	return map[string]Query{
-		createCourse: Query("INSERT INTO courses (title, subtitle, excerpt, description) VALUES ($1, $2, $3, $4) RETURNING *"),
+		createCourse: Query("INSERT INTO courses (code, name, underline, image, image_cover, excerpt, description) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *"),
 		deleteCourse: Query("UPDATE courses SET deleted_at = NOW() WHERE uuid = $1"),
 		getCourse:    Query("SELECT * FROM courses WHERE uuid = $1"),
 		listCourse:   Query("SELECT * FROM courses"),
-		updateCourse: Query("UPDATE courses SET title = $1, subtitle = $2, excerpt = $3, description = $4 WHERE uuid = $5 RETURNING *"),
+		updateCourse: Query("UPDATE courses SET name = $1, underline = $2, image = $3, image_cover = $4, excerpt = $5, description = $6 WHERE uuid = $7 RETURNING *"),
 	}
 }
