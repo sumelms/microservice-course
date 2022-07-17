c7d347493245708dc845082aa5c8a7f9e38dcb19
Ricardo Lüders <rluders@redhat.com>
2022-07-17 21:23:19 +0200

    test(course): repository test

--- internal/course/database/repository_test.go
+++ internal/course/database/repository_test.go
@@ -32,6 +32,7 @@ var (
 		UpdatedAt:   now,
 		DeletedAt:   nil,
 	}
+	emptyRows = sqlmock.NewRows([]string{})
 )
 
 func newTestDB() (*sqlx.DB, sqlmock.Sqlmock, map[string]*sqlmock.ExpectedPrepare) {
@@ -48,24 +49,33 @@ func newTestDB() (*sqlx.DB, sqlmock.Sqlmock, map[string]*sqlmock.ExpectedPrepare
 }
 
 func TestRepository_Course(t *testing.T) {
+	validRows := sqlmock.NewRows([]string{"id", "uuid", "code", "name", "underline", "image", "image_cover", "excerpt",
+		"description", "created_at", "updated_at", "deleted_at"}).
+		AddRow(course.ID, course.UUID, course.Code, course.Name, course.Underline, course.Image, course.ImageCover,
+			course.Excerpt, course.Description, course.CreatedAt, course.UpdatedAt, course.DeletedAt)
+
 	type args struct {
 		id uuid.UUID
 	}
+	
 	tests := []struct {
 		name    string
 		args    args
+		rows    *sqlmock.Rows
 		want    domain.Course
 		wantErr bool
 	}{
 		{
 			name:    "get course",
 			args:    args{id: course.UUID},
+			rows:    validRows,
 			want:    course,
 			wantErr: false,
 		},
 		{
 			name:    "course not found error",
 			args:    args{id: uuid.MustParse("6cd7a01c-ff18-4cfb-9b35-16e710115c5f")},
+			rows:    emptyRows,
 			want:    domain.Course{},
 			wantErr: true,
 		},
@@ -74,7 +84,7 @@ func TestRepository_Course(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			db, mock, stmts := newTestDB()
+			db, _, stmts := newTestDB()
 			defer db.Close()
 
 			r, err := NewRepository(db)
@@ -85,11 +95,6 @@ func TestRepository_Course(t *testing.T) {
 			if !ok {
 				t.Fatalf("prepared statement %s not found", string(getCourse))
 			}
-
-			validRows := mock.NewRows([]string{"id", "uuid", "code", "name", "underline", "image", "image_cover", "excerpt",
-				"description", "created_at", "updated_at", "deleted_at"}).
-				AddRow(course.ID, course.UUID, course.Code, course.Name, course.Underline, course.Image, course.ImageCover,
-					course.Excerpt, course.Description, course.CreatedAt, course.UpdatedAt, course.DeletedAt)
 
 			prep.ExpectQuery().WithArgs(courseUUID).WillReturnRows(validRows)
 
@@ -105,172 +110,172 @@ func TestRepository_Course(t *testing.T) {
 	}
 }
 
-//
-//func TestRepository_Courses(t *testing.T) {
-//	db, mock := mtests.NewDBMock()
-//	c := newTestCourse()
-//
-//	rows := mock.NewRows([]string{"id", "uuid", "title", "subtitle", "excerpt", "description",
-//		"created_at", "updated_at", "deleted_at"}).
-//		AddRow(c.ID, c.UUID, c.Name, c.Underline, c.Excerpt, c.Description,
-//			c.CreatedAt, c.UpdatedAt, c.DeletedAt).
-//		AddRow(2, uuid.MustParse("7aec21ad-2fa8-4ddd-b5af-073144031ecc"), c.Name, c.Underline, c.Excerpt, c.Description,
-//			c.CreatedAt, c.UpdatedAt, c.DeletedAt)
-//
-//	type fields struct {
-//		DB *sqlx.DB
-//	}
-//
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		rows    *sqlmock.Rows
-//		wantLen int
-//		wantErr bool
-//	}{
-//		{
-//			name:    "get all courses",
-//			fields:  fields{DB: db},
-//			rows:    rows,
-//			wantLen: 2,
-//			wantErr: false,
-//		},
-//		{
-//			name:    "get no courses",
-//			fields:  fields{DB: db},
-//			rows:    nil,
-//			wantLen: 0,
-//			wantErr: false,
-//		},
-//	}
-//
-//	for _, tt := range tests {
-//		tt := tt
-//		t.Run(tt.name, func(t *testing.T) {
-//			t.Parallel()
-//
-//			r := &Repository{DB: tt.fields.DB}
-//
-//			query := "SELECT \\* FROM courses WHERE deleted_at IS NULL"
-//			mock.ExpectQuery(query).WillReturnRows(rows)
-//
-//			got, err := r.Courses()
-//			if (err != nil) != tt.wantErr {
-//				t.Errorf("Courses() error = %v, wantErr %v", err, tt.wantErr)
-//				return
-//			}
-//			if len(got) != tt.wantLen {
-//				t.Errorf("Courses() got = %v, want %v", got, tt.wantLen)
-//			}
-//		})
-//	}
-//}
-//
-//func TestRepository_CreateCourse(t *testing.T) {
-//	db, mock := mtests.NewDBMock()
-//
-//	c := newTestCourse()
-//	rows := mock.NewRows([]string{"id", "uuid", "title", "subtitle", "excerpt", "description",
-//		"created_at", "updated_at", "deleted_at"}).
-//		AddRow(c.ID, c.UUID, c.Name, c.Underline, c.Excerpt, c.Description,
-//			c.CreatedAt, c.UpdatedAt, c.DeletedAt)
-//
-//	type fields struct {
-//		DB *sqlx.DB
-//	}
-//	type args struct {
-//		c *domain.Course
-//	}
-//
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		rows    *sqlmock.Rows
-//		args    args
-//		wantErr bool
-//	}{
-//		{
-//			name:    "create course",
-//			fields:  fields{DB: db},
-//			rows:    rows,
-//			args:    args{c: &c},
-//			wantErr: false,
-//		},
-//		{
-//			name:    "empty fields",
-//			fields:  fields{DB: db},
-//			rows:    nil,
-//			args:    args{c: &c},
-//			wantErr: true,
-//		},
-//	}
-//
-//	for _, tt := range tests {
-//		tt := tt
-//		t.Run(tt.name, func(t *testing.T) {
-//			t.Parallel()
-//
-//			r := &Repository{DB: tt.fields.DB}
-//
-//			query := "INSERT INTO courses \\(title, subtitle, excerpt, description\\) VALUES \\(\\$1, \\$2, \\$3, \\$4\\) RETURNING \\*"
-//			mock.ExpectQuery(query).WithArgs(c.Name, c.Underline, c.Excerpt, c.Description).WillReturnRows(rows)
-//
-//			if err := r.CreateCourse(tt.args.c); (err != nil) != tt.wantErr {
-//				t.Errorf("CreateCourse() error = %v, wantErr %v", err, tt.wantErr)
-//			}
-//		})
-//	}
-//}
-//
-//func TestRepository_UpdateCourse(t *testing.T) {
-//	db, mock := mtests.NewDBMock()
-//
-//	c := newTestCourse()
-//	rows := mock.NewRows([]string{"id", "uuid", "title", "subtitle", "excerpt", "description",
-//		"created_at", "updated_at", "deleted_at"}).
-//		AddRow(c.ID, c.UUID, c.Name, c.Underline, c.Excerpt, c.Description,
-//			c.CreatedAt, c.UpdatedAt, c.DeletedAt)
-//
-//	type fields struct {
-//		DB *sqlx.DB
-//	}
-//	type args struct {
-//		c *domain.Course
-//	}
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		args    args
-//		rows    *sqlmock.Rows
-//		wantErr bool
-//	}{
-//		{
-//			name:    "update course",
-//			fields:  fields{DB: db},
-//			args:    args{c: &c},
-//			rows:    rows,
-//			wantErr: false,
-//		},
-//		{
-//			name:    "empty course",
-//			fields:  fields{DB: db},
-//			args:    args{c: &domain.Course{}},
-//			rows:    nil,
-//			wantErr: true,
-//		},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			t.Parallel()
-//
-//			r := &Repository{DB: tt.fields.DB}
-//
-//			query := "UPDATE courses SET title = \\$1, subtitle = \\$2, excerpt = \\$3, description = \\$4 WHERE uuid = \\$5 RETURNING \\*"
-//			mock.ExpectQuery(query).WithArgs(c.Name, c.Underline, c.Excerpt, c.Description, c.UUID).WillReturnRows(rows)
-//
-//			if err := r.UpdateCourse(tt.args.c); (err != nil) != tt.wantErr {
-//				t.Errorf("UpdateCourse() error = %v, wantErr %v", err, tt.wantErr)
-//			}
-//		})
-//	}
-//}
+func TestRepository_Courses(t *testing.T) {
+	validRows := sqlmock.NewRows([]string{"id", "uuid", "code", "name", "underline", "image", "image_cover", "excerpt",
+		"description", "created_at", "updated_at", "deleted_at"}).
+		AddRow(course.ID, course.UUID, course.Code, course.Name, course.Underline, course.Image, course.ImageCover,
+			course.Excerpt, course.Description, course.CreatedAt, course.UpdatedAt, course.DeletedAt).
+		AddRow(2, uuid.MustParse("7aec21ad-2fa8-4ddd-b5af-073144031ecc"), course.Code, course.Name, course.Underline, course.Image, course.ImageCover,
+			course.Excerpt, course.Description, course.CreatedAt, course.UpdatedAt, course.DeletedAt)
+
+	tests := []struct {
+		name    string
+		rows    *sqlmock.Rows
+		wantLen int
+		wantErr bool
+	}{
+		{
+			name:    "get all courses",
+			rows:    validRows,
+			wantLen: 2,
+			wantErr: false,
+		},
+		{
+			name:    "get no courses",
+			rows:    emptyRows,
+			wantLen: 0,
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			db, _, stmts := newTestDB()
+			defer db.Close()
+
+			r, err := NewRepository(db)
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when creating the repository", err)
+			}
+			prep, ok := stmts[listCourse]
+			if !ok {
+				t.Fatalf("prepared statement %s not found", string(getCourse))
+			}
+
+			prep.ExpectQuery().WillReturnRows(tt.rows)
+
+			got, err := r.Courses()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Courses() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got) != tt.wantLen {
+				t.Errorf("Courses() got = %v, want %v", got, tt.wantLen)
+			}
+		})
+	}
+}
+
+func TestRepository_CreateCourse(t *testing.T) {
+	validRows := sqlmock.NewRows([]string{"id", "uuid", "code", "name", "underline", "image", "image_cover", "excerpt",
+		"description", "created_at", "updated_at", "deleted_at"}).
+		AddRow(course.ID, course.UUID, course.Code, course.Name, course.Underline, course.Image, course.ImageCover,
+			course.Excerpt, course.Description, course.CreatedAt, course.UpdatedAt, course.DeletedAt)
+
+	type args struct {
+		c *domain.Course
+	}
+
+	tests := []struct {
+		name    string
+		rows    *sqlmock.Rows
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "create course",
+			rows:    validRows,
+			args:    args{c: &course},
+			wantErr: false,
+		},
+		{
+			name:    "empty fields",
+			rows:    emptyRows,
+			args:    args{c: &course},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			db, _, stmts := newTestDB()
+			defer db.Close()
+
+			r, err := NewRepository(db)
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when creating the repository", err)
+			}
+			prep, ok := stmts[createCourse]
+			if !ok {
+				t.Fatalf("prepared statement %s not found", string(getCourse))
+			}
+
+			prep.ExpectQuery().WillReturnRows(tt.rows)
+
+			if err := r.CreateCourse(tt.args.c); (err != nil) != tt.wantErr {
+				t.Errorf("CreateCourse() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestRepository_UpdateCourse(t *testing.T) {
+	validRows := sqlmock.NewRows([]string{"id", "uuid", "code", "name", "underline", "image", "image_cover", "excerpt",
+		"description", "created_at", "updated_at", "deleted_at"}).
+		AddRow(course.ID, course.UUID, course.Code, course.Name, course.Underline, course.Image, course.ImageCover,
+			course.Excerpt, course.Description, course.CreatedAt, course.UpdatedAt, course.DeletedAt)
+
+	type args struct {
+		c *domain.Course
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		rows    *sqlmock.Rows
+		wantErr bool
+	}{
+		{
+			name:    "update course",
+			args:    args{c: &course},
+			rows:    validRows,
+			wantErr: false,
+		},
+		{
+			name:    "empty course",
+			args:    args{c: &domain.Course{}},
+			rows:    emptyRows,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			db, _, stmts := newTestDB()
+			defer db.Close()
+
+			r, err := NewRepository(db)
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when creating the repository", err)
+			}
+			prep, ok := stmts[updateCourse]
+			if !ok {
+				t.Fatalf("prepared statement %s not found", string(getCourse))
+			}
+
+			prep.ExpectQuery().WillReturnRows(tt.rows)
+
+			if err := r.UpdateCourse(tt.args.c); (err != nil) != tt.wantErr {
+				t.Errorf("UpdateCourse() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
