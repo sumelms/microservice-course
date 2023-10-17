7ae71991811f2f423aece502611d96f8df4d31d4
Ricardo Lüders <rluders@redhat.com>
2023-10-17 21:40:32 +0200

    fix(endpoints): delete_course

--- internal/course/database/course_queries.go
+++ internal/course/database/course_queries.go
@@ -14,14 +14,14 @@ func queriesCourse() map[string]string {
 		createCourse: `INSERT INTO 
     		courses (code, name, underline, image, image_cover, excerpt, description)
 			VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
-		deleteCourse: "UPDATE courses SET deleted_at = NOW() WHERE uuid = $1",
-		getCourse:    "SELECT * FROM courses WHERE uuid = $1",
+		deleteCourse: "UPDATE courses SET deleted_at = NOW() WHERE uuid = $1 AND deleted_at IS NULL",
+		getCourse:    "SELECT * FROM courses WHERE uuid = $1 AND deleted_at IS NULL",
 		listCourse:   "SELECT * FROM courses WHERE deleted_at IS NULL",
 		updateCourseByID: `UPDATE courses 
 			SET code = $1, name = $2, underline = $3, image = $4, image_cover = $5, excerpt = $6, description = $7 
-			WHERE uuid = $8 RETURNING *`,
+			WHERE uuid = $8 AND deleted_at IS NULL RETURNING *`,
 		updateCourseByCode: `UPDATE courses
 			SET name = $1, underline = $2, image = $3, image_cover = $4, excerpt = $5, description = $6
-			WHERE code = $7 RETURNING  *`,
+			WHERE code = $7 AND deleted_at IS NULL RETURNING  *`,
 	}
 }
